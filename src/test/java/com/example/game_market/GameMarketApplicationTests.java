8f45eec06f22dc7504771ed3bbd8bed2ca0d2797
213 <jeonyuchan926@gmail.com>
2026-06-11 14:24:37 +0900

    create basic

---
+++ src/test/java/com/example/game_market/GameMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
