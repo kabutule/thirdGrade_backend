6f7ebd74a8cc5595e310774b82a2297c05170361
213 <jeonyuchan926@gmail.com>
2026-06-11 15:13:26 +0900

    create table-JPA

---
+++ src/main/java/com/example/game_market/entity/Product.java
@@ -0,0 +1,25 @@
+package com.example.game_market.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Integer price;
+
+    private String platform;
+}
